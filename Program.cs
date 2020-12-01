d1fa8eaf0de2b0b80e67ee608b19158a04cefc99
meltoroun <58442219+meltoroun@users.noreply.github.com>
2020-12-01 23:39:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] array = new[] { 1, 2, 3, 3, 4, 5, 5 };
+            MaxIndex(array);
+        }
+        public static int MaxIndex(int []array)
+        {
+            if (array.Length == 0)
+                return -1;
+
+            int maxIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[maxIndex] < array[i])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+    }
+}
